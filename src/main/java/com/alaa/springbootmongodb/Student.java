ce7d1d550e6a1b36d4de8ff9638a78168ae4d69d
AlaaLinux <eng.alaa.janadi@gmail.com>
2022-12-03 07:29:01 +0100

    added document and id annotation to Student class

--- src/main/java/com/alaa/springbootmongodb/Student.java
+++ src/main/java/com/alaa/springbootmongodb/Student.java
@@ -1,15 +1,20 @@
 package com.alaa.springbootmongodb;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
 import java.time.ZonedDateTime;
 import java.util.List;
 
 @Data // lombok annotation to add constructor, getter and setter....
+@Document
 public class Student {
+    @Id
+    private String id;
     private String firstName;
-    private String lastNamel;
+    private String lastName;
     private String email;
     private Gender gender;
     private Address address;
