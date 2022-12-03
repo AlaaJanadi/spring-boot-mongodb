4a17ba0d03fdaa64394f92d30ab21e20a583ba64
AlaaLinux <eng.alaa.janadi@gmail.com>
2022-12-03 07:57:42 +0100

    fix Studnet and Address classes and test the database by adding a student manully

--- src/main/java/com/alaa/springbootmongodb/Address.java
+++ src/main/java/com/alaa/springbootmongodb/Address.java
@@ -1,8 +1,10 @@
 package com.alaa.springbootmongodb;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data // lombok annotation to add constructor, getter and setter....
+@AllArgsConstructor
 public class Address {
     private String country;
     private String city;
--- src/main/java/com/alaa/springbootmongodb/SpringBootMongodbApplication.java
+++ src/main/java/com/alaa/springbootmongodb/SpringBootMongodbApplication.java
@@ -1,7 +1,13 @@
 package com.alaa.springbootmongodb;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootApplication
 public class SpringBootMongodbApplication {
@@ -10,5 +16,30 @@ public class SpringBootMongodbApplication {
 
 		SpringApplication.run(SpringBootMongodbApplication.class, args);
 	}
+
+	// test to add student to database
+	@Bean
+	CommandLineRunner runner(StudentRepository repository){
+		return args -> {
+			Address address = new Address(
+					"Austria",
+					"Vienna",
+					"1190"
+			);
+
+			Student student = new Student(
+					"Alaa",
+					"Janadi",
+					"eng.alaa.janadi@gmail.com",
+					Gender.MALE,
+					address,
+					List.of("Java","C#"),
+					BigDecimal.TEN,
+					LocalDateTime.now()
+			);
+
+			repository.insert(student);
+		};
+	};
 
 }
