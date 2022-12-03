8456e8198338e3a6b50665ed46c61dcc02a70ab0
AlaaLinux <eng.alaa.janadi@gmail.com>
2022-12-03 10:21:09 +0100

    added create delete update functionality to the controller and service

--- src/main/java/com/alaa/springbootmongodb/StudentController.java
+++ src/main/java/com/alaa/springbootmongodb/StudentController.java
@@ -1,9 +1,7 @@
 package com.alaa.springbootmongodb;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,5 +14,20 @@ public class StudentController {
     @GetMapping
     public List<Student> fetchAllStudents(){
         return studentService.getAllStudents();
+    }
+
+    @PostMapping
+    public void createStudent(@RequestBody Student student){
+        studentService.addStudent(student);
+    }
+
+    @DeleteMapping("{studentId}")
+    public void deleteStudent(@PathVariable("studentId") String id){
+        studentService.deleteStudent(id);
+    }
+
+    @PutMapping("{studentId}")
+    public void updateStudent(@PathVariable("studentId") String id, @RequestBody Student newStudent){
+        studentService.updateStudent(id,newStudent);
     }
 }
--- src/main/java/com/alaa/springbootmongodb/StudentService.java
+++ src/main/java/com/alaa/springbootmongodb/StudentService.java
@@ -4,11 +4,37 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+
 @AllArgsConstructor
 @Service
 public class StudentService {
     private final StudentRepository studentRepository;
     public List<Student> getAllStudents(){
         return studentRepository.findAll();
+    }
+
+    public void addStudent(Student student){
+        studentRepository.insert(student);
+    }
+
+    public void deleteStudent(String id){
+        studentRepository.deleteById(id);
+    }
+
+    public void updateStudent(String id,Student newStudent){
+        Optional<Student> st = studentRepository.findById(id);
+        if (st.isPresent()){
+            st.get().setAddress(newStudent.getAddress());
+            st.get().setFirstName(newStudent.getFirstName());
+            st.get().setLastName(newStudent.getLastName());
+            st.get().setEmail(newStudent.getEmail());
+            st.get().setGender(newStudent.getGender());
+            st.get().setFavouriteSubjects(newStudent.getFavouriteSubjects());
+            st.get().setTotalSpentInBooks(newStudent.getTotalSpentInBooks());
+            st.get().setCreated(newStudent.getCreated());
+            studentRepository.save(st.get());
+        }
+
     }
 }
