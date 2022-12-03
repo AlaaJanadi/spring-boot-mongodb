13c3f8715f105eed8ede525082f43e5258cfa495
AlaaLinux <eng.alaa.janadi@gmail.com>
2022-12-03 09:28:09 +0100

    created StudentController and StudentService classes and apply some logic to them

--- src/main/java/com/alaa/springbootmongodb/SpringBootMongodbApplication.java
+++ src/main/java/com/alaa/springbootmongodb/SpringBootMongodbApplication.java
@@ -1,45 +1,15 @@
 package com.alaa.springbootmongodb;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.util.List;
 
 @SpringBootApplication
 public class SpringBootMongodbApplication {
-
 	public static void main(String[] args) {
 
 		SpringApplication.run(SpringBootMongodbApplication.class, args);
 	}
 
-	// test to add student to database
-	@Bean
-	CommandLineRunner runner(StudentRepository repository){
-		return args -> {
-			Address address = new Address(
-					"Austria",
-					"Vienna",
-					"1190"
-			);
 
-			Student student = new Student(
-					"Alaa",
-					"Janadi",
-					"eng.alaa.janadi@gmail.com",
-					Gender.MALE,
-					address,
-					List.of("Java","C#"),
-					BigDecimal.TEN,
-					LocalDateTime.now()
-			);
-
-			repository.insert(student);
-		};
-	};
 
 }
--- src/main/java/com/alaa/springbootmongodb/StudentRepository.java
+++ src/main/java/com/alaa/springbootmongodb/StudentRepository.java
@@ -2,5 +2,9 @@ package com.alaa.springbootmongodb;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface StudentRepository extends MongoRepository<Student,String> {
+
+
 }
